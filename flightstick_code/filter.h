d1c5b5248761b2cda63a5fe33916aff274a50ad8
Luiz Fellipe <luizopiloto@users.noreply.github.com>
2021-01-15 20:51:41 -0300

    Create filter.h

---
+++ flightstick_code/filter.h
@@ -0,0 +1,32 @@
+/*
+ * Joystick input noise reduction and axis smoothing.
+ * This file is distributed under the terms of the GNU license, (c) Luiz o Piloto 2021
+ */
+
+#ifndef _FILTER_H
+#define _FILTER_H
+#include <math.h>
+#include <stdint.h>
+
+class EMA_Filter {
+  private:
+    float alpha;     //Smoothing Factor.
+    int   sVal = 0;  //Stored value.
+    int   iVal = 0;  //Input  value.
+
+  public:
+    EMA_Filter(float al);
+    float Get_EMA(int iV);
+
+class Axis_Remap {
+  private:
+    uint8_t  range;
+    uint8_t* table = new uint8_t[range]; //Stores the lookup table created on class initialization / size = (1 byte * range) / OBS: ATmega328P have 2Kb SRAM
+
+  public:
+    Axis_Remap(uint8_t r);
+    ~Axis_Remap();
+    uint8_t output(uint8_t x);
+};
+
+#endif // _FILTER_H
